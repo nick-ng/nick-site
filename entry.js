ad9bdacffbb80bc65f0a8a8808ba52cc80f37c4c
Nick Ng <nicholas.cj.ng@gmail.com>
2017-02-05 17:37:46 +1300

    Made a webpack skeleton

---
+++ entry.js
@@ -0,0 +1,3 @@
+const content = require("./content.js");
+require("./style.css");
+document.write(content.content1);
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: './entry.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      }
+    ]
+  }
+};
