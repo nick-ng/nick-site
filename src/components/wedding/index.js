d1d9391bea2dafa1dbfd3ee3b0d51b59b3c77827
Nick Ng <n.ng@crimsoneducation.org>
2019-07-25 14:14:50 +1200

    Fixed grammer.

--- src/components/wedding/index.js
+++ src/components/wedding/index.js
@@ -5,6 +5,6 @@ import css from './styles.css';
 export default function Wedding() {
   return (<div className={css.container}>
     <h1>Sylvia and Nick</h1>
-    <h3>We’re got married on the 23rd of September 2018!</h3>
+    <h3>We are now married!</h3>
   </div>);
 }
