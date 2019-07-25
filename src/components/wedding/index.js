b3d1c148640c4c98f728c685a690bdfa4c4fa5c2
Nick Ng <n.ng@crimsoneducation.org>
2019-07-25 14:00:34 +1200

    Changed page since we got married.

--- src/components/wedding/index.js
+++ src/components/wedding/index.js
@@ -5,6 +5,6 @@ import css from './styles.css';
 export default function Wedding() {
   return (<div className={css.container}>
     <h1>Sylvia and Nick</h1>
-    <h3>We’re getting married!</h3>
+    <h3>We’re got married on the 23rd of September 2018!</h3>
   </div>);
 }
