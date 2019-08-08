c0563f5c112d02f13eabd94765da4565a2f6c235
Nick Ng <n.ng@crimsoneducation.org>
2019-08-08 15:14:05 +1200

    Add time to anniversary display.

--- src/components/wedding/index.js
+++ src/components/wedding/index.js
@@ -2,9 +2,53 @@ import React from 'react';
 
 import css from './styles.css';
 
-export default function Wedding() {
-  return (<div className={css.container}>
-    <h1>Sylvia and Nick</h1>
-    <h3>We are now married!</h3>
-  </div>);
+const weddingAnniversary = new Date('2018-09-23T00:00:00+1200');
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
+
+const timeUntilAnniversary = () => {
+  const currentYear = (new Date()).getFullYear();
+  let nextAnniversary = new Date(weddingAnniversary).setFullYear(currentYear);
+
+  for (let i = 0; i < 10000000; i++) { // eslint-disable-line no-plusplus
+    if (nextAnniversary > new Date()) {
+      break;
+    }
+    nextAnniversary = new Date(weddingAnniversary).setFullYear(currentYear + i);
+  }
+  const millisecondsRemaining = nextAnniversary - new Date();
+
+  return numberWithCommas(millisecondsRemaining);
+};
+
+class Wedding extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      milliseconds: '',
+    };
+
+    this.updateMilliseconds = this.updateMilliseconds.bind(this);
+
+    setInterval(this.updateMilliseconds, 11);
+  }
+
+  updateMilliseconds() {
+    this.setState({
+      milliseconds: timeUntilAnniversary(),
+    });
+  }
+
+  render() {
+    const { milliseconds } = this.state;
+    return (<div className={css.container}>
+      <h1>Sylvia and Nick</h1>
+      <h3>{`Only ${milliseconds} milliseconds until our next wedding anniversary!`}</h3>
+    </div>);
+  }
+}
+
+export default Wedding;
