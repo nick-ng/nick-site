2b0edaaa068bc00b8def29c8de389df7a6f04812
Nick Ng <nick.ng@fergusapp.com>
2020-01-08 13:03:08 +1300

    Fixed indent Changed environmental variable name.

--- webpack.config.js
+++ webpack.config.js
@@ -2,55 +2,55 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const plugins = [
-  new HtmlWebpackPlugin({
-    title: 'Nick Ng',
-    favicon: `${__dirname}/favicon.ico`,
-    template: './index.html',
-    inject: 'true',
-  }),
+    new HtmlWebpackPlugin({
+        title: 'Nick Ng',
+        favicon: `${__dirname}/favicon.ico`,
+        template: './index.html',
+        inject: 'true',
+    }),
 ];
 if (process.env.NODE_ENV === 'production') {
-  console.log('Production build!'); // eslint-disable-line no-console
-  plugins.push(new webpack.optimize.UglifyJsPlugin());
-  plugins.push(new webpack.DefinePlugin({
-    'process.env': {
-      NODE_ENV: JSON.stringify('production'),
-    },
-  }));
+    console.log('Production build!'); // eslint-disable-line no-console
+    plugins.push(new webpack.optimize.UglifyJsPlugin());
+    plugins.push(new webpack.DefinePlugin({
+        'process.env': {
+            NODE_ENV: JSON.stringify('production'),
+        },
+    }));
 }
 
 module.exports = {
-  devtool: 'source-map',
-  entry: './src/entry.js',
-  output: {
-    path: `${__dirname}/dist`,
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.css$/,
-        loader: 'style-loader',
-      },
-      {
-        test: /\.css$/,
-        loader: 'css-loader',
-        query: {
-          modules: true,
-          localIdentName: '[name]__[local]__[hash:base64:6]',
-        },
-      },
-      {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader',
-      },
-    ],
-  },
-  plugins,
+    devtool: 'source-map',
+    entry: './src/entry.js',
+    output: {
+        path: `${__dirname}/dist`,
+        filename: 'bundle.js',
+        publicPath: '/',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: 'style-loader',
+            },
+            {
+                test: /\.css$/,
+                loader: 'css-loader',
+                query: {
+                    modules: true,
+                    localIdentName: '[name]__[local]__[hash:base64:6]',
+                },
+            },
+            {
+                test: /\.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader',
+            },
+        ],
+    },
+    plugins,
 };
