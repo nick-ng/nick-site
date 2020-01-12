75e4d8703f98a387ebcba48ace6a5c0c4658f8b2
Nick Ng <nick.ng@fergusapp.com>
2020-01-12 13:22:54 +1300

    removed redux

--- src/entry.js
+++ src/entry.js
@@ -1,28 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { createStore, compose, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-// import thunk from 'redux-thunk';
 
 import Router from './routes';
-import rootReducer from './stores';
-
-// const store = createStore(
-//   rootReducer,
-//   compose(
-//     applyMiddleware(thunk),
-//     window.devToolsExtension ? window.devToolsExtension() : f => f,
-//   ),
-// );
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <div>
-//       {Router(store)}
-//     </div>
-//   </Provider>,
-//   document.getElementById('root'),
-// );
 
 ReactDOM.render(
     <Router />,
