d2cdbd740516947be6be05b2198ef08bff0c52cf
Nick Ng <nick.ng@fergusapp.com>
2020-01-12 13:13:38 +1300

    Upgraded React Router
    - Switched to react-router-dom
    - Removed react-router-redux
    - Tidied up routes
    - Change where CSS is loaded.

--- src/entry.js
+++ src/entry.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, compose, applyMiddleware } from 'redux';
+// import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
 
-import getRouter from './routes';
+import Router from './routes';
 import rootReducer from './stores';
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.devToolsExtension ? window.devToolsExtension() : f => f,
-  ),
-);
+// const store = createStore(
+//   rootReducer,
+//   compose(
+//     applyMiddleware(thunk),
+//     window.devToolsExtension ? window.devToolsExtension() : f => f,
+//   ),
+// );
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <div>
+//       {Router(store)}
+//     </div>
+//   </Provider>,
+//   document.getElementById('root'),
+// );
 
 ReactDOM.render(
-  <Provider store={store}>
-    <div>
-      {getRouter(store)}
-    </div>
-  </Provider>,
-  document.getElementById('root'),
+    <Router />,
+    document.getElementById('root'),
 );
--- src/stores/index.js
+++ src/stores/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import { routerReducer as routing } from 'react-router-redux';
 import posts from './posts';
 import postEditor from './post-editor';
 
 export default combineReducers({
-  routing,
   posts,
   postEditor,
 });
