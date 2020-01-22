17306e7e47e85bfb6e7d0648c3eeaeaaba4e8689
Nick Ng <nick-ng@users.noreply.github.com>
2020-01-22 19:51:03 +1300

    Update server.js

--- server.js
+++ server.js
@@ -18,9 +18,8 @@ app.use(compression());
 // https to http redirect
 app.use((req, res, next) => {
     console.log('req.subdomains', req.subdomains);
-    return next();
     if (req.subdomains.length === 1) {
-        switch req.subdomains[0].toLowerCase() {
+        switch (req.subdomains[0].toLowerCase()) {
             case 'yt':
                 return res.redirect('https://www.youtube.com/feed/subscriptions');
             default:
