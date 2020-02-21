1de5c9ffd27ea9f466b7a50b30dfa233278837ba
Nick Ng <nicholas.cj.ng@gmail.com>
2020-02-22 12:23:56 +1300

    Fixed prettier issues.

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 
 import css from './styles.css';
 
-const Header = () => (
-    <div className={css.container}>
-        Sylvia &amp; Nick
-    </div>
-);
+const Header = () => <div className={css.container}>Sylvia &amp; Nick</div>;
 
 export default Header;
