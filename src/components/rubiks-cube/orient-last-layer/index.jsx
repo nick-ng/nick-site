8e0d4ee775092f80b609b836095138fe10cc85ce
Nick Ng <nicholas.cj.ng@gmail.com>
2020-05-26 21:29:56 +1200

    Added videos for oll.

--- src/components/rubiks-cube/orient-last-layer/index.jsx
+++ src/components/rubiks-cube/orient-last-layer/index.jsx
@@ -1,15 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import css from '../styles.css';
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+`;
 
-const OLL_GFYCATS = {
-    1: {
-        gfycatId: '',
+const OllSets = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+`;
+
+const OllSet = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+`;
+
+const OllVideos = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+`;
+
+const OLL_VIDEOS = [
+    {
+        case: 'All Edges Oriented',
+        videos: ['21', '22', '23', '24', '25', '26', '27'],
     },
-};
+    // {
+    //     case: 'All Corners Oriented',
+    //     videos: [],
+    // },
+    {
+        case: 'Square Shapes',
+        videos: ['05', '06'],
+    },
+    {
+        case: 'P Shapes',
+        videos: ['31', '32', '43', '44'],
+    },
+];
 
 const PermuteLastLayerPage = () => (
-    <div className={css.container}>
+    <Container>
         <h1>OLLs</h1>
         <p>
             Original videos{' '}
@@ -17,23 +52,27 @@ const PermuteLastLayerPage = () => (
                 Rubik's Cube: All 57 OLL Algorithms &amp; Finger Tricks
             </a>{' '}
             by <a href="https://www.youtube.com/channel/UCqTVfT9JQqhA6_Hi_h_h97Q">J Perm</a>.
-            Generated with <a href="https://gfycat.com/">gfycat</a>
         </p>
-        <div className={css.imageContainer}>
-            {Object.keys(OLL_GFYCATS)
-                .sort()
-                .map(pll => {
-                    const { gfycatId } = OLL_GFYCATS[pll];
-                    return (
-                        <img
-                            key={`img-${gfycatId}`}
-                            src={`https://thumbs.gfycat.com/${gfycatId}.webp`}
-                            className={css.picture}
-                        />
-                    );
-                })}
-        </div>
-    </div>
+        <OllSets>
+            {OLL_VIDEOS.map(ollSet => (
+                <OllSet key={ollSet.case}>
+                    <h3>{ollSet.case}</h3>
+                    <OllVideos>
+                        {ollSet.videos.map(videoNumber => (
+                            <video
+                                key={`${ollSet.case}-${videoNumber}`}
+                                loop
+                                autoPlay
+                                muted
+                                width={400}
+                                src={`/oll/${videoNumber}.mp4`}
+                            />
+                        ))}
+                    </OllVideos>
+                </OllSet>
+            ))}
+        </OllSets>
+    </Container>
 );
 
 export default PermuteLastLayerPage;
