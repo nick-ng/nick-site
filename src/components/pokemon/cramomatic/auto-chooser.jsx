060365d4ef0888cc589908f1e90cc01e5c15c589
Nick Ng <nick.ng@fergusapp.com>
2020-08-30 09:06:22 +1200

    Changed replaceAll to replace

--- src/components/pokemon/cramomatic/auto-chooser.jsx
+++ src/components/pokemon/cramomatic/auto-chooser.jsx
@@ -6,191 +6,175 @@ import { ingredientNames, ingredients, recipes } from './item-data';
 import { validRemainingItems } from './utils';
 
 const Container = styled.div`
-    justify-self: center;
+  justify-self: center;
 `;
 
 const Recipe = styled.div`
-    margin-top: 1em;
-    display: grid;
-    justify-content: start;
-    grid-template-columns: repeat(4, auto);
-    gap: 0.5em;
+  margin-top: 1em;
+  display: grid;
+  justify-content: start;
+  grid-template-columns: repeat(4, auto);
+  gap: 0.5em;
 `;
 
 const RecipeHeading = styled.div`
-    font-weight: bold;
-    text-align: ${props => props.textAlign || 'left'};
+  font-weight: bold;
+  text-align: ${(props) => props.textAlign || 'left'};
 `;
 
 const RecipeItem = styled.div`
-    text-align: ${props => props.textAlign || 'left'};
+  text-align: ${(props) => props.textAlign || 'left'};
 `;
 
 const BASE_VALUE = 1000000;
 
-const randomItem = items => items[Math.floor(Math.random() * items.length)];
+const randomItem = (items) => items[Math.floor(Math.random() * items.length)];
 
-const getItemPrice = count => BASE_VALUE - count;
+const getItemPrice = (count) => BASE_VALUE - count;
 
 const makeItem = (
-    desiredItem,
-    inventory,
-    startingPrice = Infinity,
-    count = 10
+  desiredItem,
+  inventory,
+  startingPrice = Infinity,
+  count = 10
 ) => {
-    let bestPrice = startingPrice;
-    let bestItems = [];
+  let bestPrice = startingPrice;
+  let bestItems = [];
 
-    const prices = ingredientNames.reduce((prev, name) => {
-        prev[name] = getItemPrice(inventory[name]?.count || 0);
-        return prev;
-    }, {});
+  const prices = ingredientNames.reduce((prev, name) => {
+    prev[name] = getItemPrice(inventory[name]?.count || 0);
+    return prev;
+  }, {});
 
-    const excludedItems = Object.keys(inventory).filter(
-        item => inventory[item]?.exclude
+  const excludedItems = Object.keys(inventory).filter(
+    (item) => inventory[item]?.exclude
+  );
+
+  const recipe = recipes[desiredItem];
+
+  for (let n = 0; n < count; n++) {
+    const tempItems = [];
+    const itemsA = validRemainingItems(
+      recipe.range,
+      [],
+      [desiredItem, ...excludedItems]
+    ).filter((ingredient) =>
+      recipe?.types.includes(ingredients[ingredient].type)
     );
-
-    const recipe = recipes[desiredItem];
-
-    for (let n = 0; n < count; n++) {
-        const tempItems = [];
-        const itemsA = validRemainingItems(
-            recipe.range,
-            [],
-            [desiredItem, ...excludedItems]
-        ).filter(ingredient =>
-            recipe?.types.includes(ingredients[ingredient].type)
-        );
-        tempItems.push(randomItem(itemsA));
-        for (let m = 0; m < 2; m++) {
-            const itemsB = validRemainingItems(recipe.range, tempItems, [
-                desiredItem,
-                ...excludedItems,
-            ]);
-            tempItems.push(randomItem(itemsB));
-        }
-        const itemsC = validRemainingItems(recipe.range, tempItems, [
-            desiredItem,
-            ...excludedItems,
-        ]);
-        itemsC.sort((a, b) => prices[a] - prices[b]);
-        tempItems.push(itemsC[0]);
-        const resultPrice = tempItems.reduce((prev, item) => {
-            return prev + prices[item];
-        }, 0);
-
-        if (resultPrice <= bestPrice) {
-            bestPrice = resultPrice;
-            bestItems = tempItems;
-        }
+    tempItems.push(randomItem(itemsA));
+    for (let m = 0; m < 2; m++) {
+      const itemsB = validRemainingItems(recipe.range, tempItems, [
+        desiredItem,
+        ...excludedItems,
+      ]);
+      tempItems.push(randomItem(itemsB));
     }
+    const itemsC = validRemainingItems(recipe.range, tempItems, [
+      desiredItem,
+      ...excludedItems,
+    ]);
+    itemsC.sort((a, b) => prices[a] - prices[b]);
+    tempItems.push(itemsC[0]);
+    const resultPrice = tempItems.reduce((prev, item) => {
+      return prev + prices[item];
+    }, 0);
 
-    return {
-        bestPrice,
-        bestItems,
-    };
+    if (resultPrice <= bestPrice) {
+      bestPrice = resultPrice;
+      bestItems = tempItems;
+    }
+  }
+
+  return {
+    bestPrice,
+    bestItems,
+  };
 };
 
 const AutoChooser = ({ desiredItem, inventory }) => {
-    const [price, setPrice] = useState(Infinity);
-    const [items, setItems] = useState([]);
+  const [price, setPrice] = useState(Infinity);
+  const [items, setItems] = useState([]);
 
-    useEffect(() => {
-        if (Object.keys(recipes).includes(desiredItem)) {
-            const { bestPrice, bestItems } = makeItem(
-                desiredItem,
-                inventory,
-                Infinity,
-                50
-            );
-            setPrice(bestPrice);
-            setItems(bestItems);
-        } else {
-            setPrice(Infinity);
-            setItems([]);
-        }
-    }, [desiredItem, inventory]);
+  useEffect(() => {
+    if (Object.keys(recipes).includes(desiredItem)) {
+      const { bestPrice, bestItems } = makeItem(
+        desiredItem,
+        inventory,
+        Infinity,
+        50
+      );
+      setPrice(bestPrice);
+      setItems(bestItems);
+    } else {
+      setPrice(Infinity);
+      setItems([]);
+    }
+  }, [desiredItem, inventory]);
 
-    return (
-        <Container>
-            <h2>Auto Chooser</h2>
-            {Object.keys(recipes).includes(desiredItem) ? (
-                <div>
-                    <p>{`Making ${startCase(desiredItem)} (${
-                        recipes[desiredItem].range[0]
-                    } - ${recipes[desiredItem].range[1]})`}</p>
-                </div>
-            ) : (
-                <h3>Choose an item</h3>
-            )}
-            {Object.keys(recipes).includes(desiredItem) && (
-                <button
-                    onClick={() => {
-                        const temp = makeItem(
-                            desiredItem,
-                            inventory,
-                            price,
-                            500
-                        );
-                        if (
-                            temp.bestPrice <= price &&
-                            temp.bestItems.length === 4
-                        ) {
-                            setPrice(temp.bestPrice);
-                            setItems(temp.bestItems);
-                        }
-                    }}
+  return (
+    <Container>
+      <h2>Auto Chooser</h2>
+      {Object.keys(recipes).includes(desiredItem) ? (
+        <div>
+          <p>{`Making ${startCase(desiredItem)} (${
+            recipes[desiredItem].range[0]
+          } - ${recipes[desiredItem].range[1]})`}</p>
+        </div>
+      ) : (
+        <h3>Choose an item</h3>
+      )}
+      {Object.keys(recipes).includes(desiredItem) && (
+        <button
+          onClick={() => {
+            const temp = makeItem(desiredItem, inventory, price, 500);
+            if (temp.bestPrice <= price && temp.bestItems.length === 4) {
+              setPrice(temp.bestPrice);
+              setItems(temp.bestItems);
+            }
+          }}
+        >
+          Try Again
+        </button>
+      )}
+      {items.length > 0 && (
+        <Recipe>
+          <RecipeHeading>Item</RecipeHeading>
+          <RecipeHeading textAlign="right">Value</RecipeHeading>
+          <RecipeHeading textAlign="center">Type</RecipeHeading>
+          <RecipeHeading textAlign="right">##</RecipeHeading>
+          {items.map((item, i) => (
+            <React.Fragment key={`${item}${i}`}>
+              <RecipeItem>
+                <a
+                  href={`https://www.serebii.net/itemdex/${item
+                    .replace(/_/g, '')
+                    .replace(/'/g, '')}.shtml`}
+                  target="_blank"
                 >
-                    Try Again
-                </button>
-            )}
-            {items.length > 0 && (
-                <Recipe>
-                    <RecipeHeading>Item</RecipeHeading>
-                    <RecipeHeading textAlign="right">Value</RecipeHeading>
-                    <RecipeHeading textAlign="center">Type</RecipeHeading>
-                    <RecipeHeading textAlign="right">##</RecipeHeading>
-                    {items.map((item, i) => (
-                        <React.Fragment key={`${item}${i}`}>
-                            <RecipeItem>
-                                <a
-                                    href={`https://www.serebii.net/itemdex/${item
-                                        .replaceAll('_', '')
-                                        .replaceAll("'", '')}.shtml`}
-                                    target="_blank"
-                                >
-                                    {startCase(item)}
-                                </a>
-                            </RecipeItem>
-                            <RecipeItem textAlign="right">
-                                {ingredients[item].value}
-                            </RecipeItem>
-                            <RecipeItem textAlign="center">
-                                {startCase(ingredients[item].type)}
-                            </RecipeItem>
-                            <RecipeItem textAlign="right">
-                                {inventory[item].count}
-                            </RecipeItem>
-                        </React.Fragment>
-                    ))}
-                    <RecipeHeading>Totals</RecipeHeading>
-                    <RecipeHeading textAlign="right">
-                        {items.reduce(
-                            (prev, item) => prev + ingredients[item].value,
-                            0
-                        )}
-                    </RecipeHeading>
-                    <div />
-                    <RecipeHeading textAlign="right">
-                        {items.reduce(
-                            (prev, item) => prev + inventory[item].count,
-                            0
-                        )}
-                    </RecipeHeading>
-                </Recipe>
-            )}
-        </Container>
-    );
+                  {startCase(item)}
+                </a>
+              </RecipeItem>
+              <RecipeItem textAlign="right">
+                {ingredients[item].value}
+              </RecipeItem>
+              <RecipeItem textAlign="center">
+                {startCase(ingredients[item].type)}
+              </RecipeItem>
+              <RecipeItem textAlign="right">{inventory[item].count}</RecipeItem>
+            </React.Fragment>
+          ))}
+          <RecipeHeading>Totals</RecipeHeading>
+          <RecipeHeading textAlign="right">
+            {items.reduce((prev, item) => prev + ingredients[item].value, 0)}
+          </RecipeHeading>
+          <div />
+          <RecipeHeading textAlign="right">
+            {items.reduce((prev, item) => prev + inventory[item].count, 0)}
+          </RecipeHeading>
+        </Recipe>
+      )}
+    </Container>
+  );
 };
 
 export default AutoChooser;
--- src/components/rubiks-cube/timer/scramble.jsx
+++ src/components/rubiks-cube/timer/scramble.jsx
@@ -7,105 +7,108 @@ import css from './styles.css';
 const STICKER_SIZE = 30;
 const STICKER_SIZE_SMALL = 15;
 const COLOR_MAP = {
-    U: 'white',
-    R: 'red',
-    F: 'green',
-    D: 'yellow',
-    L: 'orange',
-    B: 'blue',
+  U: 'white',
+  R: 'red',
+  F: 'green',
+  D: 'yellow',
+  L: 'orange',
+  B: 'blue',
 };
 
 const Scramble = styled.div`
-    display: flex;
-    justify-content: center;
-    font-size: 2.5em;
+  display: flex;
+  justify-content: center;
+  font-size: 2.3em;
 
-    @media screen and (max-device-width: 1280px) {
-        font-size: 1.5em;
-    }
+  @media screen and (max-device-width: 1280px) {
+    font-size: 1.5em;
+  }
 `;
 
 const CubeNet = styled.div`
-    margin-bottom: 10px;
-    justify-content: center;
-    display: grid;
-    grid-template-columns: repeat(4, ${STICKER_SIZE * 3}px);
-    grid-template-rows: repeat(3, ${STICKER_SIZE * 3}px);
-    gap: 5px;
+  margin-bottom: 10px;
+  justify-content: center;
+  display: grid;
+  grid-template-columns: repeat(4, ${STICKER_SIZE * 3}px);
+  grid-template-rows: repeat(3, ${STICKER_SIZE * 3}px);
+  gap: 5px;
 
-    @media screen and (max-device-width: 1280px) {
-        grid-template-columns: repeat(4, ${STICKER_SIZE_SMALL * 3}px);
-        grid-template-rows: repeat(3, ${STICKER_SIZE_SMALL * 3}px);
-        gap: 2px;
-    }
+  @media screen and (max-device-width: 1280px) {
+    grid-template-columns: repeat(4, ${STICKER_SIZE_SMALL * 3}px);
+    grid-template-rows: repeat(3, ${STICKER_SIZE_SMALL * 3}px);
+    gap: 2px;
+  }
 `;
 
 const Face = styled.div`
-    display: grid;
+  display: grid;
 
-    grid-column-start: ${props => props.column};
-    grid-row-start: ${props => props.row};
-    grid-template-columns: repeat(3, ${STICKER_SIZE}px);
-    grid-template-rows: repeat(3, ${STICKER_SIZE}px);
-    @media screen and (max-device-width: 1280px) {
-        grid-template-columns: repeat(3, ${STICKER_SIZE_SMALL}px);
-        grid-template-rows: repeat(3, ${STICKER_SIZE_SMALL}px);
-    }
+  grid-column-start: ${(props) => props.column};
+  grid-row-start: ${(props) => props.row};
+  grid-template-columns: repeat(3, ${STICKER_SIZE}px);
+  grid-template-rows: repeat(3, ${STICKER_SIZE}px);
+  @media screen and (max-device-width: 1280px) {
+    grid-template-columns: repeat(3, ${STICKER_SIZE_SMALL}px);
+    grid-template-rows: repeat(3, ${STICKER_SIZE_SMALL}px);
+  }
 `;
 
 const Sticker = styled.div`
-    background-color: ${props => COLOR_MAP[props.scolor]};
-    box-sizing: border-box;
-    border: 1px solid black;
-    width: ${STICKER_SIZE}px;
-    height: ${STICKER_SIZE}px;
-    @media screen and (max-device-width: 1280px) {
-        width: ${STICKER_SIZE_SMALL}px;
-        height: ${STICKER_SIZE_SMALL}px;
-    }
+  background-color: ${(props) => COLOR_MAP[props.scolor]};
+  box-sizing: border-box;
+  border: 1px solid black;
+  width: ${STICKER_SIZE}px;
+  height: ${STICKER_SIZE}px;
+  @media screen and (max-device-width: 1280px) {
+    width: ${STICKER_SIZE_SMALL}px;
+    height: ${STICKER_SIZE_SMALL}px;
+  }
 `;
 
 const CubeScrambleHelper = ({ cubeString, scramble }) => {
-    const [uFace, rFace, fFace, dFace, lFace, bFace] = chunk(cubeString.split(''), 9);
-    return (
-        <div className={css.cubeScrambleHelper}>
-            {cubeString.length === 54 && (
-                <CubeNet>
-                    <Face column={2} row={1}>
-                        {uFace.map((sticker, i) => (
-                            <Sticker key={`u${i}`} scolor={sticker} />
-                        ))}
-                    </Face>
-                    <Face column={1} row={2}>
-                        {lFace.map((sticker, i) => (
-                            <Sticker key={`l${i}`} scolor={sticker} />
-                        ))}
-                    </Face>
-                    <Face column={2} row={2}>
-                        {fFace.map((sticker, i) => (
-                            <Sticker key={`f${i}`} scolor={sticker} />
-                        ))}
-                    </Face>
-                    <Face column={3} row={2}>
-                        {rFace.map((sticker, i) => (
-                            <Sticker key={`r${i}`} scolor={sticker} />
-                        ))}
-                    </Face>
-                    <Face column={4} row={2}>
-                        {bFace.map((sticker, i) => (
-                            <Sticker key={`b${i}`} scolor={sticker} />
-                        ))}
-                    </Face>
-                    <Face column={2} row={3}>
-                        {dFace.map((sticker, i) => (
-                            <Sticker key={`d${i}`} scolor={sticker} />
-                        ))}
-                    </Face>
-                </CubeNet>
-            )}
-            <Scramble>{scramble}</Scramble>
-        </div>
-    );
+  const [uFace, rFace, fFace, dFace, lFace, bFace] = chunk(
+    cubeString.split(''),
+    9
+  );
+  return (
+    <div className={css.cubeScrambleHelper}>
+      {cubeString.length === 54 && (
+        <CubeNet>
+          <Face column={2} row={1}>
+            {uFace.map((sticker, i) => (
+              <Sticker key={`u${i}`} scolor={sticker} />
+            ))}
+          </Face>
+          <Face column={1} row={2}>
+            {lFace.map((sticker, i) => (
+              <Sticker key={`l${i}`} scolor={sticker} />
+            ))}
+          </Face>
+          <Face column={2} row={2}>
+            {fFace.map((sticker, i) => (
+              <Sticker key={`f${i}`} scolor={sticker} />
+            ))}
+          </Face>
+          <Face column={3} row={2}>
+            {rFace.map((sticker, i) => (
+              <Sticker key={`r${i}`} scolor={sticker} />
+            ))}
+          </Face>
+          <Face column={4} row={2}>
+            {bFace.map((sticker, i) => (
+              <Sticker key={`b${i}`} scolor={sticker} />
+            ))}
+          </Face>
+          <Face column={2} row={3}>
+            {dFace.map((sticker, i) => (
+              <Sticker key={`d${i}`} scolor={sticker} />
+            ))}
+          </Face>
+        </CubeNet>
+      )}
+      <Scramble>{scramble}</Scramble>
+    </div>
+  );
 };
 
 export default CubeScrambleHelper;
