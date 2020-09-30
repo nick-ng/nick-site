3294d7dd2d87db8d408d61d7731f441949af8356
Nick Ng <nick.ng@fergusapp.com>
2020-10-01 09:44:38 +1300

    made dev endpoint.

---
+++ src_server/router/dev.js
@@ -0,0 +1,11 @@
+module.exports = (router) => {
+  router.get('/dev/:status', async (req, res, next) => {
+    const { status } = req.params;
+    try {
+      const statusInt = status - 0;
+      res.sendStatus(statusInt);
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  });
+};
--- src_server/router/index.js
+++ src_server/router/index.js
@@ -1,6 +1,7 @@
 const bookmarkRouter = require('./bookmarks');
 const boxclickerRouter = require('./boxclicker');
 const countdownRouter = require('./countdowns');
+const dev = require('./dev');
 const foreignStorageRouter = require('./foreign-storage');
 const rubiksCubeRouter = require('./rubiks-cube');
 const weddingPhotoRouter = require('./wedding-photos');
@@ -9,6 +10,7 @@ const applyRouters = (router) => {
   bookmarkRouter(router);
   boxclickerRouter(router);
   countdownRouter(router);
+  dev(router);
   foreignStorageRouter(router);
   rubiksCubeRouter(router);
   weddingPhotoRouter(router);
