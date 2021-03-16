614a3214702a190e8a416be71e5a43cedbf94161
Nick Ng <nicholas.cj.ng@gmail.com>
2021-03-16 23:28:10 +1300

    aaaaab

--- knexfile.js
+++ knexfile.js
@@ -28,7 +28,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: `${process.env.DATABASE_URL}`,
+    connection: `${process.env.DATABASE_URL}?ssl=true`,
     pool: {
       min: 2,
       max: 10,
