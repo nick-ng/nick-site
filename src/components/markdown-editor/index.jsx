2e78c91a7092cc4791c7658acdd5e0369f230f0a
Nick Ng <nicholas.cj.ng@gmail.com>
2021-04-10 23:37:00 +1200

    oops

--- src/components/markdown-editor/index.jsx
+++ src/components/markdown-editor/index.jsx
@@ -197,7 +197,6 @@ export default function MarkdownEditor({ notesOnly }) {
                           status,
                           publishAt,
                           uri,
-                          ...newData,
                         }
                       );
                     }
