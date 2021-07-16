a057a0b591eb0660783f061875e286c324b217fb
Nick Ng <nick-ng@users.noreply.github.com>
2021-07-16 12:00:39 +1200

    Update redirect.js

--- src_server/middleware/redirect.js
+++ src_server/middleware/redirect.js
@@ -2,6 +2,10 @@ module.exports = app => {
     app.use((req, res, next) => {
         if (req.subdomains.length === 1) {
             switch (req.subdomains[0].toLowerCase()) {
+                case 'poe':
+                    return res.redirect(
+                        'https://poe.pux.one'
+                    );
                 case 'yt':
                     return res.redirect(
                         'https://www.youtube.com/feed/subscriptions'
