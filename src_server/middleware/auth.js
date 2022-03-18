3f19238028b70dc156e4c994c2a4e425eefdebc7
Nick Ng <nicholas.cj.ng@gmail.com>
2022-03-19 00:21:57 +1300

    remove unnecessary guest key

--- src_server/middleware/auth.js
+++ src_server/middleware/auth.js
@@ -1,45 +1,33 @@
 const db = require('../sql-database');
 
-module.exports = app => {
-    app.use(async (req, res, next) => {
-        const adminKey = req.header('x-admin-key');
-        if (!adminKey) {
-            return next();
+module.exports = (app) => {
+  app.use(async (req, res, next) => {
+    const adminKey = req.header('x-admin-key');
+    if (!adminKey) {
+      return next();
+    }
+
+    const identities = [];
+    if (process.env.NICK_ADMIN_KEY) {
+      identities.push({
+        username: 'Nick',
+        key: process.env.NICK_ADMIN_KEY,
+      });
+    }
+
+    await Promise.all(
+      identities.map(async (identity) => {
+        if (adminKey === identity.key) {
+          try {
+            const user = await db.user.getOrAddUser(identity.username);
+            res.locals.user = user;
+          } catch (e) {
+            console.error(`Trouble when getting ${identity.username} info`, e);
+          }
         }
+      })
+    );
 
-        const identities = [];
-        if (process.env.NICK_ADMIN_KEY) {
-            identities.push({
-                username: 'Nick',
-                key: process.env.NICK_ADMIN_KEY,
-            });
-        }
-
-        await Promise.all(
-            identities.map(async identity => {
-                if (adminKey === identity.key) {
-                    try {
-                        const user = await db.user.getOrAddUser(
-                            identity.username
-                        );
-                        res.locals.user = user;
-                    } catch (e) {
-                        console.error(
-                            `Trouble when getting ${identity.username} info`,
-                            e
-                        );
-                    }
-                }
-            })
-        );
-
-        if (process.env.GUEST_KEY && adminKey === process.env.GUEST_KEY) {
-            res.locals.user = {
-                id: 0,
-                username: 'guest',
-            };
-        }
-
-        return next();
-    });
+    return next();
+  });
 };
