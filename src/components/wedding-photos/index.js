677feaaacd744251a4ab409d200eff2efaccf37f
Nick Ng <nicholas.cj.ng@gmail.com>
2022-03-18 17:54:29 +1300

    reduce default quality of full wedding photos

--- src/components/wedding-photos/index.js
+++ src/components/wedding-photos/index.js
@@ -9,54 +9,55 @@ import css from './styles.css';
 // https://www.contentful.com/developers/docs/references/images-api/#/reference
 
 const thumbnailParams = '?fm=jpg&q=80&fit=fill&w=500&h=500&f=faces';
+const viewParams = '?fm=jpg&q=75';
 
 export default class WeddingPhotos extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            loaded: false,
-            photos: [],
-        };
-    }
+    this.state = {
+      loaded: false,
+      photos: [],
+    };
+  }
 
-    async componentDidMount() {
-        this.setState({
-            loaded: false,
-        });
-        const res = await axios.get('/api/wedding_photos');
-        const photos = res.data;
-        this.setState({
-            loaded: true,
-            photos,
-        });
-    }
+  async componentDidMount() {
+    this.setState({
+      loaded: false,
+    });
+    const res = await axios.get('/api/wedding_photos');
+    const photos = res.data;
+    this.setState({
+      loaded: true,
+      photos,
+    });
+  }
 
-    render() {
-        const { loaded, photos } = this.state;
-        return (
-            <div className={css.container}>
-                <h2>Wedding Photos</h2>
-                {loaded ? (
-                    <div className={css.thumbnailGrid}>
-                        {photos.map(photo => {
-                            const url = photo.file.url;
-                            const description = photo.description;
-                            return (
-                                <a key={`a-${url}`} href={url}>
-                                    <img
-                                        key={`img-${url}`}
-                                        src={`${url}${thumbnailParams}`}
-                                        alt={description}
-                                    />
-                                </a>
-                            );
-                        })}
-                    </div>
-                ) : (
-                    <Loading />
-                )}
-            </div>
-        );
-    }
+  render() {
+    const { loaded, photos } = this.state;
+    return (
+      <div className={css.container}>
+        <h2>Wedding Photos</h2>
+        {loaded ? (
+          <div className={css.thumbnailGrid}>
+            {photos.map((photo) => {
+              const url = photo.file.url;
+              const description = photo.description;
+              return (
+                <a key={`a-${url}`} href={`https:${url}${viewParams}`}>
+                  <img
+                    key={`img-${url}`}
+                    src={`https:${url}${thumbnailParams}`}
+                    alt={description}
+                  />
+                </a>
+              );
+            })}
+          </div>
+        ) : (
+          <Loading />
+        )}
+      </div>
+    );
+  }
 }
