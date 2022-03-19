aee06f23840035aeac84e2c98fb095c75688f779
Nick Ng <nicholas.cj.ng@gmail.com>
2022-03-20 10:55:35 +1300

    added a minimum ratio to the wedding album preview so it looks better on phones

--- src/components/wedding-photos/photo-preview.jsx
+++ src/components/wedding-photos/photo-preview.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
+const TARGET_PX = 920000;
+
 const Container = styled.div`
   position: fixed;
   top: 0;
@@ -41,6 +43,8 @@ export default function PhotoPreview(props) {
 
   const [width, setWidth] = useState(80);
   const [height, setHeight] = useState(80);
+  const [pixelsW, setPixelsW] = useState(90);
+  const [pixelsH, setPixelsH] = useState(90);
 
   useEffect(() => {
     const scale = 0.8;
@@ -49,8 +53,28 @@ export default function PhotoPreview(props) {
     const windowWidth = window.innerWidth;
     const windowHeight = window.innerHeight;
 
-    setWidth(Math.min(ratio * windowHeight * scale, windowWidth * scale));
-    setHeight(Math.min((windowWidth / ratio) * scale, windowHeight * scale));
+    const newWidth = Math.min(
+      ratio * windowHeight * scale,
+      windowWidth * scale
+    );
+    const newHeight = Math.min(
+      (windowWidth / ratio) * scale,
+      windowHeight * scale
+    );
+
+    setWidth(Math.floor(newWidth));
+    setHeight(Math.floor(newHeight));
+
+    const pixels = newWidth * newHeight;
+    const ratioB = Math.sqrt(TARGET_PX / pixels);
+
+    if (ratioB > 1) {
+      setPixelsW(Math.floor(newWidth * ratioB));
+      setPixelsH(Math.floor(newHeight * ratioB));
+    } else {
+      setPixelsW(Math.floor(newWidth));
+      setPixelsH(Math.floor(newHeight));
+    }
   }, [imageWidth, imageHeight]);
 
   return (
@@ -69,9 +93,7 @@ export default function PhotoPreview(props) {
         style={{ width: `${width + 10}px`, height: `${height + 10}px` }}
       >
         <img
-          src={`${imageUrl}?fm=jpg&q=85&fit=fill&w=${Math.floor(
-            width
-          )}&h=${Math.floor(height)}`}
+          src={`${imageUrl}?fm=jpg&q=85&fit=fill&w=${pixelsW}&h=${pixelsH}`}
           style={{ width: `${width}px`, height: `${height}px` }}
         />
       </PictureLink>
