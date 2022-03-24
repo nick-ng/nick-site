3a1e7b384de207d0bdada89a7edb009a44c18074
Nick Ng <nicholas.cj.ng@gmail.com>
2022-03-24 22:20:30 +1300

    made the default sort order a finite number so you can put photos after it

--- src/components/wedding-photos/utils.js
+++ src/components/wedding-photos/utils.js
@@ -2,10 +2,15 @@ export const addTagsToPhotos = (photos, photoTags) => {
   return photos.map((photo) => {
     const matchingTags = photoTags.filter((tag) => tag.uri === photo.file.url);
 
+    const sortOrder =
+      matchingTags.length > 0
+        ? Math.min(...matchingTags.map((a) => a.sortOrder))
+        : 1000000;
+
     return {
       ...photo,
       tags: matchingTags,
-      sortOrder: Math.min(...matchingTags.map((a) => a.sortOrder), Infinity),
+      sortOrder,
     };
   });
 };
