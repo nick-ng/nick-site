1906029ea682c67775eb4fb9bd3137f557029915
Nick Ng <nicholas.cj.ng@gmail.com>
2022-03-26 17:54:33 +1300

    clarified the Mastermind rules and made the ui easier to use

--- src/components/mastermind/index.jsx
+++ src/components/mastermind/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 
 import GuessAccuracy from './guess-accuracy';
@@ -17,7 +17,7 @@ const Instructions = styled.p`
   margin-top: 0;
 `;
 
-const MaxRangeInput = styled.input`
+const MaxNumberInput = styled.input`
   width: 3em;
 `;
 
@@ -58,52 +58,69 @@ const Guess = styled.div`
 `;
 
 export default function Mastermind() {
-  const [maxRange, setMaxRange] = useState(6);
-  const [answer, setAnswer] = useState(getAnswer(1, parseInt(maxRange, 10)));
+  const [maxNumber, setMaxNumber] = useState(6);
+  const [answer, setAnswer] = useState(getAnswer(1, parseInt(maxNumber, 10)));
   const [guesses, setGuesses] = useState([]);
   const [currentGuess, setCurrentGuess] = useState(['', '', '', '']);
   const [isCorrect, setIsCorrect] = useState(false);
   const [showReset, setShowReset] = useState(false);
 
+  const answerInputs = [useRef(null), useRef(null), useRef(null), useRef(null)];
+
+  const maxNumberLength = maxNumber.toString().length;
+
   const submitGuess = () => {
     if (currentGuess.every((a) => a.length > 0)) {
       const guessAccuracy = checkGuess(currentGuess, answer);
       setGuesses((prev) =>
-        prev.concat([{ guess: currentGuess, guessAccuracy }])
+        prev.concat([
+          { guess: currentGuess.map((a) => parseInt(a, 10)), guessAccuracy },
+        ])
       );
       setCurrentGuess(['', '', '', '']);
-      if (guessAccuracy.correct === 4) {
+      if (guessAccuracy.correct === answer.length) {
         setIsCorrect(true);
         setShowReset(true);
       }
+      answerInputs[0].current.focus();
     }
   };
 
   const resetGame = () => {
     setGuesses([]);
-    setAnswer(getAnswer(1, parseInt(maxRange, 10)));
+    setAnswer(getAnswer(1, parseInt(maxNumber, 10)));
     setShowReset(false);
     setIsCorrect(false);
+    setCurrentGuess(['', '', '', '']);
   };
+
+  useEffect(() => {
+    resetGame();
+  }, [maxNumber]);
 
   return (
     <Container>
       <h2>Mastermind</h2>
       <Instructions>
-        Enter the numbers between 1 and{' '}
-        <MaxRangeInput
+        Enter numbers between 1 and{' '}
+        <MaxNumberInput
           type="number"
-          value={maxRange}
+          value={maxNumber}
           onChange={(e) => {
-            setMaxRange(e.target.value);
+            setMaxNumber(e.target.value);
             setShowReset(true);
           }}
         />{' '}
         in the boxes below then press enter or the OK button.
       </Instructions>
-      <Instructions>⚫ means a number is correct place.</Instructions>
+      <Instructions>Numbers may be repeated.</Instructions>
+      <Instructions>A ⚫ hint means a number is correct place.</Instructions>
       <Instructions>
-        ⚪ means a number is in the answer but in the wrong place.
+        A ⚪ hint means a number is in the answer but in the wrong place.
+      </Instructions>
+      <Instructions>
+        The hints don't indicate <em>which</em> number is correct. Only how many
+        are correct/nearly correct.
       </Instructions>
       {showReset && (
         <button type="button" onClick={resetGame}>
@@ -137,12 +154,23 @@ export default function Mastermind() {
                     <input
                       type="tel"
                       value={guess}
-                      onChange={(e) => {
+                      ref={answerInputs[i]}
+                      onChange={() => {}}
+                      onInput={(e) => {
                         const value = e.target.value;
+
                         setCurrentGuess((prev) => {
                           prev[i] = value;
                           return [...prev];
                         });
+
+                        if (
+                          value.length >= maxNumberLength ||
+                          parseInt(`${value}0`, 10) > maxNumber
+                        ) {
+                          answerInputs[(i + 1) % 4].current.focus();
+                          answerInputs[(i + 1) % 4].current.select();
+                        }
                       }}
                     />
                   </td>
