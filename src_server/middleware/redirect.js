49350ee06e967ce4eaf4b36829052b9c0b0ee7f9
Nick Ng <nicholas.cj.ng@gmail.com>
2022-08-09 17:46:15 +1200

    redirect roborally and robot-race

--- src_server/middleware/redirect.js
+++ src_server/middleware/redirect.js
@@ -1,18 +1,17 @@
-module.exports = app => {
-    app.use((req, res, next) => {
-        if (req.subdomains.length === 1) {
-            switch (req.subdomains[0].toLowerCase()) {
-                case 'poe':
-                    return res.redirect(
-                        'https://poe.pux.one'
-                    );
-                case 'yt':
-                    return res.redirect(
-                        'https://www.youtube.com/feed/subscriptions'
-                    );
-                default:
-            }
-        }
-        return next();
-    });
+module.exports = (app) => {
+  app.use((req, res, next) => {
+    if (req.subdomains.length === 1) {
+      switch (req.subdomains[0].toLowerCase()) {
+        case 'poe':
+          return res.redirect('https://poe.pux.one');
+        case 'roborally':
+        case 'robot-race':
+          return res.redirect('https://robot-race.pux.one/');
+        case 'yt':
+          return res.redirect('https://www.youtube.com/feed/subscriptions');
+        default:
+      }
+    }
+    return next();
+  });
 };
