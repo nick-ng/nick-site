60fb379510451383b6b26da6b2a6a714c032826f
Nick Ng <nicholas.cj.ng@gmail.com>
2023-01-01 09:09:10 +1300

    add button to mark a day's push-ups as done

--- src/components/resolution-2023/index.jsx
+++ src/components/resolution-2023/index.jsx
@@ -7,6 +7,19 @@ const DAYS_DONE_STORE = 'nick-ng-days-done-store';
 
 const months = new Array(12).fill(0).map((_, i) => i);
 
+const dateOptions = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+};
+
+const getDateString = (date) => {
+  return `${date.getFullYear()}-${(date.getMonth() + 1)
+    .toString()
+    .padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+};
+
 const StyledResolution2023 = styled.div``;
 
 export default function Resolution2023() {
@@ -15,8 +28,7 @@ export default function Resolution2023() {
 
   useEffect(() => {
     (async () => {
-      const temp = await getArray(DAYS_DONE_STORE);
-      console.log('temp', temp);
+      setDaysDone(await getArray(DAYS_DONE_STORE));
     })();
   }, []);
 
@@ -27,6 +39,31 @@ export default function Resolution2023() {
         <li>Do push-ups equal to the sum of the month and the date.</li>
       </ul>
       <p>Push-ups to do today: {today.getDate() + today.getMonth() + 1}</p>
+      <button
+        onClick={() => {
+          const dateString = getDateString(today);
+          if (daysDone.includes(dateString)) {
+            return;
+          }
+
+          const newDaysDone = daysDone.concat([dateString]);
+
+          setArray(DAYS_DONE_STORE, newDaysDone);
+          setDaysDone(newDaysDone);
+        }}
+      >
+        Push-ups for {today.toLocaleDateString('en-NZ', dateOptions)} done
+      </button>
+      <p>Push-ups done:</p>
+      <ul>
+        {daysDone
+          .sort((a, b) => new Date(a) - new Date(b))
+          .map((dateString) => (
+            <li key={dateString}>
+              {new Date(dateString).toLocaleDateString('en-NZ', dateOptions)}
+            </li>
+          ))}
+      </ul>
     </StyledResolution2023>
   );
 }
