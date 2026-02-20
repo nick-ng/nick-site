5f53c53746e37bfa33af5fba43a60f81d42f0b86
pux <nick-ng@users.noreply.github.com>
2026-02-21 10:53:35 +1300

    fix: spell require correctly

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const compression = require('compression');
-const cors = requore('cors');
+const cors = require('cors');
 const path = require('path');
 const http = require('http');
 
